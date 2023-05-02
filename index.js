a7d5fccf579a11bc9d3d604babdeec032fda1a29
FarjanaAkterLaila <farjanaakter7275@gmail.com>
2023-05-02 16:40:57 +0600

    data update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =require ('express');
+const cors =require("cors");
 const app=express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 const checfs = require('./data/checfs.json')
 app.get('/',(req,res)=>{
@@ -10,12 +12,6 @@ app.get('/checfs',(req,res)=>{
     res.send(checfs);
 })
 
-app.get('/checfs/:id',(req,res)=>{
-    const id = req.params.id;
-    console.log(id);
-    const selectedchecfs = checfs.find(n => n._id === id)
-    res.send(selectedchecfs)
-})
 app.listen(port,()=>{
     console.log(`Food api on ${port}`)
 })
