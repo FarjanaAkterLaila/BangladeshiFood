b552f1f617e93a63ea4b3497e518578fd6cced31
FarjanaAkterLaila <farjanaakter7275@gmail.com>
2023-05-04 17:52:21 +0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ res.send('Food is running');
 app.get('/checfs',(req,res)=>{
     res.send(checfs);
 })
-
+app.get('/checfs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = checfs.find(n => n._id == id);
+    res.send(selectedNews)
+})
 app.listen(port,()=>{
     console.log(`Food api on ${port}`)
 })
